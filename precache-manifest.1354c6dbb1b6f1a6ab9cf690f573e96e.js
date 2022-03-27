a06456ffd2ec711363a1c691c2d7f2e4ab6d202c
RQie <ruiqire@163.com>
2022-03-28 00:33:45 +0800

    init project

---
+++ precache-manifest.1354c6dbb1b6f1a6ab9cf690f573e96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8399006a1589b658d16cf903661616be",
+    "url": "/account-book/index.html"
+  },
+  {
+    "revision": "a602e2d2881693d7eb5e",
+    "url": "/account-book/static/css/main.9ac8f82d.chunk.css"
+  },
+  {
+    "revision": "89ab52047c0374a2f938",
+    "url": "/account-book/static/js/2.e321ec4e.chunk.js"
+  },
+  {
+    "revision": "e4e6e48a9ecea75dc1966a170f826888",
+    "url": "/account-book/static/js/2.e321ec4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a602e2d2881693d7eb5e",
+    "url": "/account-book/static/js/main.8249cc06.chunk.js"
+  },
+  {
+    "revision": "c5a3ef79ca2a7d42dee1",
+    "url": "/account-book/static/js/runtime-main.11bbe495.js"
+  }
+]);
